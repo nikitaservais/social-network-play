fe171f84df6e12aff4d03813cabec8c03b3fb594
nservais <nikita@lab-box.studio>
2024-01-02 18:21:46 +0100

    feat: perfect mock

--- app/models/Dao.scala
+++ app/models/Dao.scala
@@ -4,72 +4,155 @@ import java.time.ZonedDateTime
 
 object Dao {
   var users: Seq[User] = Seq(
-    User("Paul", "emperor")
+    User("Paul Atreides", "muaddib"),
+    User("Duncan Idaho", "swordmaster"),
+    User("Lady Jessica", "bene-gesserit"),
+    User("Thufir Hawat", "mentat"),
+    User("Baron Harkonnen", "harkonnen"),
+    User("Alia Atreides", "st.alia"),
+    User("Stilgar", "fremen_leader"),
+    User("Gurney Halleck", "troubadour_warrior"),
+    User("Dr. Yueh", "suk_doctor"),
+    User("Princess Irulan", "corrin_princess"),
+    User("Chani", "fremen"),
+    User("Liet-Kynes", "planetologist"),
+    User("Feyd-Rautha", "harkonnen_heir"),
+    User("Piter De Vries", "twisted_mentat"),
+    User("Shaddam IV", "padishah_emperor")
   )
   var posts: Seq[Post] = Seq(
     Post(
       id = 1,
-      user = "Paul",
+      user = "Paul Atreides",
       likes = Seq(
-        Like("Paul", ZonedDateTime.parse("2023-12-29T12:32:12Z")),
-        Like("user1", ZonedDateTime.parse("2023-12-29T12:32:12Z")),
-        Like("user2", ZonedDateTime.parse("2023-12-29T12:32:12Z"))
+        Like("Duncan Idaho", utils.randomDateTime()),
+        Like("Lady Jessica", utils.randomDateTime()),
+        Like("Thufir Hawat", utils.randomDateTime()),
+        Like("Gurney Halleck", utils.randomDateTime())
       ),
-      imagePath = "images/test-image.jpeg",
-      createdAt = ZonedDateTime.parse("2025-12-29T11:00:11Z"),
-      description = "My beautiful image",
+      imagePath = "images/arriving-arrakis.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "First steps on the sands of Dune. The twin suns witness our resolve. We're ready to face what comes next, together. #Arrakis #NewBeginnings",
       comments = Seq(
-        Comment("test", "Wow!"),
-        Comment("bother1", "Cool this is !"),
-        Comment("bother2", "Cool this is !"),
-        Comment("bother3", "Cool this is !")
+        Comment("Duncan Idaho", "The spice must flow!"),
+        Comment("Lady Jessica", "Be careful, Paul."),
+        Comment("Thufir Hawat", "We must be vigilant."),
+        Comment("Gurney Halleck", "We stand together."),
+        Comment("Alia Atreides", "The future is uncertain."),
+        Comment("Stilgar", "We trust in Muad'Dib."),
+        Comment("Dr. Yueh", "May the Maker guide us."),
+        Comment("Princess Irulan", "The universe is watching."),
+        Comment("Chani", "We are the children of Dune.")
       )
     ),
     Post(
       id = 2,
-      user = "Paul",
-      likes = Seq(Like("test", ZonedDateTime.parse("2023-12-29T12:32:12Z"))),
-      imagePath = "images/test-image.jpeg",
-      createdAt = ZonedDateTime.parse("2001-12-29T11:00:11Z"),
-      description = "My beautiful image",
+      user = "Baron Harkonnen",
+      likes = Seq(Like("Thufir Hawat", utils.randomDateTime())),
+      imagePath = "images/baron-plot.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "Plotting the next big move. Power waits for no one. #HarkonnenRise #GiediPrime",
       comments = Seq(
-        Comment("user2", "Wow!")
+        Comment("Thufir Hawat", "The Harkonnens are not to be trusted.")
       )
     ),
     Post(
       id = 3,
-      user = "Paul",
-      likes = Seq(Like("Paul", ZonedDateTime.parse("2023-12-29T12:32:12Z"))),
-      imagePath = "images/test-image.jpeg",
-      createdAt = ZonedDateTime.parse("2023-12-29T11:00:11Z"),
-      description = "My beautiful image",
+      user = "Stilgar",
+      likes = Seq(
+        Like("Paul Atreides", utils.randomDateTime()),
+        Like("Alia Atreides", utils.randomDateTime())
+      ),
+      imagePath = "images/arrakeen-siege.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "We stand united - a storm brewing against the horizon. Arrakis will be free. #Fremen #ArrakisRising",
       comments = Seq(
-        Comment("user2", "Wow!")
+        Comment("Paul Atreides", "We fight for Arrakis."),
+        Comment("Alia Atreides", "For the future of our people.")
       )
     ),
     Post(
       id = 4,
-      user = "Paul",
-      likes = Seq(Like("Paul", ZonedDateTime.parse("2023-12-29T12:32:12Z"))),
-      imagePath = "images/test-image.jpeg",
-      createdAt = ZonedDateTime.parse("2023-12-29T11:00:11Z"),
-      description = "My beautiful image",
+      user = "Gurney Halleck",
+      likes = Seq(
+        Like("Duncan Idaho", utils.randomDateTime()),
+        Like("Paul Atreides", utils.randomDateTime())
+      ),
+      imagePath = "images/training.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "Another day, another training session. The desert's heat forges the strongest warriors. #TrainingDay #ArrakisTough",
       comments = Seq(
-        Comment("user2", "Wow!")
+        Comment("Duncan Idaho", "We are ready."),
+        Comment("Paul Atreides", "Your leadership is invaluable, Gurney.")
       )
     ),
     Post(
       id = 5,
-      user = "Paul",
-      likes = Seq(
-        Like("Paul", ZonedDateTime.parse("2023-12-29T12:32:12Z")),
-        Like("user2", ZonedDateTime.parse("2023-12-29T12:32:12Z"))
-      ),
-      imagePath = "images/test-image.jpeg",
-      createdAt = ZonedDateTime.parse("2023-12-29T11:00:11Z"),
-      description = "My beautiful image",
+      user = "Dr. Yueh",
+      likes = Seq(Like("Baron Harkonnen", utils.randomDateTime())),
+      imagePath = "images/yueh.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "A choice that weighs heavily. The path ahead is fraught with shadows. #DifficultDecisions #TheDoctor'sDilemma",
       comments = Seq(
-        Comment("user2", "Wow!")
+        Comment("Baron Harkonnen", "I trust you")
+      )
+    ),
+    Post(
+      id = 6,
+      user = "Princess Irulan",
+      likes = Seq(Like("Lady Jessica", utils.randomDateTime())),
+      imagePath = "images/irulan.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "Documenting the epic saga. Words carry the weight of worlds. #MuadDibChronicles #HistoryUnfolds",
+      comments = Seq(
+        Comment("Lady Jessica", "History will remember.")
+      )
+    ),
+    Post(
+      id = 7,
+      user = "Lady Jessica",
+      likes = Seq(
+        Like("Paul Atreides", utils.randomDateTime()),
+        Like("Alia Atreides", utils.randomDateTime())
+      ),
+      imagePath = "images/jessica.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "Embracing the trials of Arrakis. The Bene Gesserit way lights our path. #StrengthAndWisdom #MotherOfMuadDib",
+      comments = Seq(
+        Comment(
+          "Paul Atreides",
+          "Your guidance is the light in the sandstorm."
+        ),
+        Comment("Alia Atreides", "Mother, your strength shapes us.")
+      )
+    ),
+    Post(
+      id = 8,
+      user = "Thufir Hawat",
+      likes = Seq(
+        Like("Paul Atreides", utils.randomDateTime()),
+        Like("Lady Jessica", utils.randomDateTime())
+      ),
+      imagePath = "images/thufir.png",
+      createdAt = utils.randomDateTime(),
+      description =
+        "Analyzing and strategizing every angle. Arrakis won't know what hit it. #MentatMind #HouseAtreidesStrategist",
+      comments = Seq(
+        Comment(
+          "Paul Atreides",
+          "Your wisdom is the cornerstone of our house."
+        ),
+        Comment(
+          "Lady Jessica",
+          "Thufir, your loyalty and insight are invaluable."
+        )
       )
     )
   )
--- app/utils/UtilsDateTime.scala
+++ app/utils/UtilsDateTime.scala
@@ -1,8 +1,8 @@
 package utils
 
-import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
-
+import java.time.{Instant, LocalDateTime, ZoneId, ZonedDateTime}
+import java.util.concurrent.ThreadLocalRandom
 val dateTimeFormat =
   DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd")
@@ -13,4 +13,16 @@ def displayDateTime(dateTime: ZonedDateTime): String = {
 
 def displayDate(dateTime: ZonedDateTime): String = {
   dateTime.format(dateFormat)
+}
+
+def randomDateTime(): ZonedDateTime = {
+  val start = LocalDateTime.of(2020, 1, 1, 0, 0)
+  val end = LocalDateTime.of(2023, 12, 31, 23, 59)
+  val random = ThreadLocalRandom
+    .current()
+    .nextLong(
+      start.toEpochSecond(ZoneId.systemDefault().getRules.getOffset(start)),
+      end.toEpochSecond(ZoneId.systemDefault().getRules.getOffset(end))
+    )
+  ZonedDateTime.ofInstant(Instant.ofEpochSecond(random), ZoneId.systemDefault())
 }
